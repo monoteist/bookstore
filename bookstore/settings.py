0639ada5c9fb673b2792c13168d3a204f2d0ccfc
= <i.ezerbaev97@gmail.com>
2021-11-01 18:03:21 +0300

    add bootstrap and static

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -128,7 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -137,3 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 
 
 urlpatterns = [
+    path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
 ]
