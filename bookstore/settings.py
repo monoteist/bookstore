43cbbdeb0b1455293b2e54bb3352c0a8c31dd359
= <i.ezerbaev97@gmail.com>
2021-11-03 21:41:10 +0300

    add enviroment variables

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z)tn0v5lkkis!ydk@=#nech)dq@n*6b=u^mi7mh$i8+z!lf6p&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
