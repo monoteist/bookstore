01c4222be20b1c78f5d0c5cdb1d74b8aa3822164
= <i.ezerbaev97@gmail.com>
2021-11-04 23:39:58 +0300

    books app create| model Book create | book list and book detail views add

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.urls import reverse
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self): # new
+        return reverse('book_detail', args=[str(self.id)])
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     path('', include('pages.urls')),
+    path('books/', include('books.urls')),
+    
 ]
